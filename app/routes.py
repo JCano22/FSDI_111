ad50f222425aceff8e07da98af1bde5054dc7b0b
Jorge Cano <coronero@gmail.com>
2023-05-17 17:01:52 -0700

    FSDI-111

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask import Flask  # from the flask module import the Flask class
 app = Flask(__name__)  # instantiate the Flask class into app (now an object)
 
 
-@app.get("/aboutme")  # Flask decorator that maps a route to a view function
+@app.get("/")  # Flask decorator that maps a route to a view function
 def index():  # a wrapped function is a view function in flask
     me = {
         "first_name": "JorgeM",
