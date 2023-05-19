5c4241ee274a4db1cf5ecd665c3442b9ccdd3d56
Jorge Cano <coronero@gmail.com>
2023-05-18 19:27:09 -0700

    FSDI-111, sandbox . db added

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,2 @@
-#! /usr/bin/venv python3
-# -*- coding: utf8 -*-
-"""Sample Hello world Flask app"""
+
 
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import Flask  # from the flask module import the Flask class
+from flask import Flask, request # from the flask module import the Flask class
 
 app = Flask(__name__)  # instantiate the Flask class into app (now an object)
 
+myList = []
 
 @app.get("/")  # Flask decorator that maps a route to a view function
 def index():  # a wrapped function is a view function in flask
@@ -11,3 +12,13 @@ def index():  # a wrapped function is a view function in flask
         "hobbies": "music"
     } 
     return me  # when you return a dictionary from flask view function flask will automatically convert it to JSON
+
+@app.post("/")
+def create_entry():
+    raw_data = request.json
+    myList.append(raw_data)
+    return "", 204
+
+@app.get("/entries")
+def get_entries():
+    return myList
